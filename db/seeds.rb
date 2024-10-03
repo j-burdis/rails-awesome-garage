452c6eb3b83de4d95fa8c1d799619f0c928b860a
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-03 08:44:01 +0100

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Owner.create!(nickname: "Jon")
+Owner.create!(nickname: "Paul")
+Car.create!(brand: "Nissan", model: "Qashqai", year: 2014, fuel: "Diesel", owner_id: 1)
+Car.create!(brand: "Ferrari", model: "Testarossa", year: 1984, fuel: "Petrol", owner_id: 2)
