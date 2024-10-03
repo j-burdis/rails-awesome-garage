90d06e319583a8ddefd42f6975afbed1b530b17d
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-03 11:08:33 +0100

    amend model associations

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,5 +1,4 @@
 class Favourite < ApplicationRecord
   belongs_to :car
-  belongs_to :owner
   validates :car_id, uniqueness: { scope: :owner_id, message: "has already been favourited by you" }
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,4 @@
 class Owner < ApplicationRecord
   has_many :cars
-  has_many :reviews, through: :cars
-  has_many :favourites, through: :cars
   validates :nickname, presence: true, uniqueness: true
 end
